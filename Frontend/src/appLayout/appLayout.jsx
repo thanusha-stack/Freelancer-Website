4e20580aa5d76e8fe841c92f90f9046357794294
thanusha-stack <thanusha13062006@gmail.com>
2024-10-19 10:26:55 +0530

    add files

---
+++ Frontend/src/appLayout/appLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Router, Routes, Route } from "react-router-dom";
+
+function AppLayout() {
+
+    return (
+        <h1>Hello World!</h1>
+    )
+  }
+  
+  export default AppLayout;
