3ae7d16706355ddae9842a4cba7d44c316c1fb67
thanusha-stack <thanusha13062006@gmail.com>
2024-10-19 10:48:29 +0530

    color and screen length added

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,css}",  // Ensure it covers all file types used
-  ],
+module.exports = {
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    screens:{sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        primary: '#1D4ED8', // Blue 600
+        secondary: '#64748B', // Gray 500
+        accent: '#10B981', // Green 500
+        muted: '#6B7280', // Neutral Gray 400
+        background: '#F3F4F6', // Gray 100
+        warning: '#F59E0B', // Amber 500
+        danger: '#EF4444', // Red 500
+        info: '#3B82F6', // Blue 500
+        dark: '#111827', // Gray 900
+        light: '#F9FAFB', // Gray 50
+        DEFAULT: '#6A7B8C'
+      }
+    },
   },
   plugins: [],
 }
+
