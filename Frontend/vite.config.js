a77807e728bb5edc37622c01574cc357aff8073a
thanusha-stack <thanusha13062006@gmail.com>
2024-11-07 06:25:10 +0530

    vite.config changed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Makes the server accessible externally
+    port: 3000,
+  },  
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg']
 })
